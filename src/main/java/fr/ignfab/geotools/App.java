90c2390db1d34d9b6a97df22d941cd230cf7aab5
Pierre-Yves Rollo <pierre-yves.rollo@ign.fr>
2023-12-11 16:21:03 +0100

    First commit

---
+++ src/main/java/fr/ignfab/geotools/App.java
@@ -0,0 +1,13 @@
+package fr.ignfab.geotools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
