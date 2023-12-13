f93a8a954d5a15842bc332e11d2e231f09df626b
Pierre-Yves Rollo <pierre-yves.rollo@ign.fr>
2023-12-13 14:33:03 +0100

    Geotools 30.1

--- src/main/java/fr/ignfab/geotools/App.java
+++ src/main/java/fr/ignfab/geotools/App.java
@@ -5,14 +5,13 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
-import org.geotools.GML;
-import org.geotools.GML.Version;
+import org.geotools.wfs.GML;
+import org.geotools.wfs.GML.Version;
 import org.geotools.referencing.CRS;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
-import org.opengis.feature.simple.SimpleFeature;
-
+import org.geotools.api.feature.simple.SimpleFeature;
+import org.geotools.api.referencing.crs.CoordinateReferenceSystem;
 
 /**
  * Hello world!
@@ -24,11 +23,13 @@ public class App
     {
 		CoordinateReferenceSystem wgs84Crs = CRS.decode("EPSG:4326");
 
-        String url = "https://wxs.ign.fr/topographie/geoportail/wfs?SERVICE=WFS&VERSION=1.1.0"+
-//        String url = "https://data.geopf.fr/wfs/ows?SERVICE=WFS&VERSION=2.0.0"+
+//        String url = "https://wxs.ign.fr/topographie/geoportail/wfs?SERVICE=WFS&VERSION=1.1.0"+
+        String url = "https://data.geopf.fr/wfs/ows?SERVICE=WFS&VERSION=2.0.0"+
             "&TYPENAME=BDTOPO_V3:cimetiere&REQUEST=GetFeature"+
             "&SRSNAME=EPSG:2154&BBOX=650556.55754621070,6859402.33482602000,651068.55754621070,6859914.33482602000,EPSG:2154"+
             "&STARTINDEX=0&MAXFEATURES=1000";
+
+//        url = "https://data.geopf.fr/wfs/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=BDTOPO_V3:cimetiere&STARTINDEX=0&COUNT=1000000&SRSNAME=urn:ogc:def:crs:EPSG::4326&BBOX=48.71180421753307144,2.17865471308054914,48.96896620898196772,2.44789335494747862,urn:ogc:def:crs:EPSG::4326";
 
 /*        String url = "https://data.geopf.fr/wfs/ows?SERVICE=WFS&VERSION=2.0.0"+
             "&TYPENAME=BDTOPO_V3:cimetiere&REQUEST=GetFeature"+
@@ -50,6 +51,8 @@ public class App
 		SimpleFeatureCollection features = gml.decodeFeatureCollection(rawDataStream);
 
         if(features != null) {
+            System.out.println("Number of features : " + String.valueOf(features.size()));
+
             SimpleFeatureIterator it = features.features();
             while (it.hasNext()) {
                 System.out.println("NEXT FEATURE");
